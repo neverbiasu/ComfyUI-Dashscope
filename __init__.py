00348f0a3d49eb990f2682eedc91a4a7cfc4767b
Fayche <1751162157@qq.com>
2024-12-17 14:57:38 +0800

    fix: Repair ComfyUI/issues/6074

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 from .node import DashscopeLLMLoader
 
 
-NODE_CLASS_MAPPINGS = {"DashscopeLLMLoader": DashscopeLLMLoader}
+NODE_CLASS_MAPPINGS = {
+    "DashscopeLLMLoader": DashscopeLLMLoader,
+}
 
-NODE_DISPLAY_NAME_MAPPINGS = {"DashscopeLLMLoader": "Dashscope LLM Loader"}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "DashscopeLLMLoader": "Dashscope LLM Loader",
+}
