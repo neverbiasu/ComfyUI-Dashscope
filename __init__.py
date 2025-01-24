ee670dc3c9740868fbdc0fa189c3019634bdaf6b
neverbiasu <1751162157@qq.com>
2025-01-24 19:19:39 +0800

    feat: Add nodes mapping

--- __init__.py
+++ __init__.py
@@ -1,14 +1,24 @@
-from .node import DashscopeLLMLoader, DashscopeVLMLoader, DashscopeModelCaller
+from .node import (
+    DashscopeLLMLoader,
+    DashscopeVLMLoader,
+    DashscopeModelCaller,
+    DashscopeOCRCaller,
+    DashscopeEmoCaller,
+)
 
 
 NODE_CLASS_MAPPINGS = {
     "DashscopeLLMLoader": DashscopeLLMLoader,
     "DashscopeVLMLoader": DashscopeVLMLoader,
     "DashscopeModelCaller": DashscopeModelCaller,
+    "DashscopeOCRCaller": DashscopeOCRCaller,
+    "DashscopeEmoCaller": DashscopeEmoCaller,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DashscopeLLMLoader": "Dashscope LLM Loader",
     "DashscopeVLMLoader": "Dashscope VLM Loader",
     "DashscopeModelCaller": "Dashscope Model Caller",
+    "DashscopeOCRCaller": "Dashscope OCR Caller",
+    "DashscopeEmoCaller": "Dashscope Emotion Caller",
 }
